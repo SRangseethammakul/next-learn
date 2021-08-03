400b01bb0e9fdd06646aea5d12012c5cf4bca9e2
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2021-08-03 12:40:16 +0700

    set format

--- src/pages/api/users/[id].js
+++ src/pages/api/users/[id].js
@@ -1,13 +1,13 @@
-import { userInfo } from './users'
+import { userInfo } from "./users";
 
 export default (req, res) => {
-  const id = req.query && req.query.id
+  const id = req.query && req.query.id;
 
-  const user = userInfo.find((i) => i.id === +id)
+  const user = userInfo.find((i) => i.id === +id);
 
   if (user) {
-    res.status(200).json(user)
+    res.status(200).json(user);
   } else {
-    res.status(404).send('Something went wrong')
+    res.status(404).send("Something went wrong");
   }
-}
+};
